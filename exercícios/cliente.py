7b0c2088a25389626e097a70a392a93700e97138
Lucas-Fiche <lucas.fiche.u.borges@gmail.com>
2025-09-19 16:53:54 -0300

    Remove arquivos e pastas ignorados do rastreamento

--- exercícios/cliente.py
+++
@@ -1,9 +0,0 @@
-class Cliente:
-    def __init__(self, nome, idade, cpf,):
-        self.nome = nome
-        self.idade = idade
-        self.cpf = cpf
-        self.fidelidade = False
-
-
-cliente1 = Cliente('Lucas', '24', '123.456.789-12')
