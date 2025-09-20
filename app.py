1065e343f8966cb35f76abe399bede2f786ee2ab
Lucas-Fiche <lucas.fiche.u.borges@gmail.com>
2025-09-20 11:09:22 -0300

    Finaliza o projeto OO e adiciona o requirements.txt para consumação de API

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
 from modelos.restaurante import Restaurante
+from modelos.cardapio.bebida import Bebida
+from modelos.cardapio.prato import Prato
+from modelos.cardapio.sobremesa import Sobremesa
 
 restaurante1 = Restaurante('Praça', 'Gourmet')
-restaurante1.receber_avaliacao('Lucas', 10)
-restaurante1.receber_avaliacao('Caio', 8)
-restaurante1.receber_avaliacao('José', 2)
+
+bebida1 = Bebida('Suco de Laranja', 5.0 , 'Grande')
+bebida1.aplicar_desconto()
+
+prato1 = Prato('Pãozinho', 2.0, 'Pão australiando com manteiga')
+prato1.aplicar_desconto()
+
+sobremesa1 = Sobremesa('Sorvete', 4.5, 'Gelado', 'Pequeno', 'Sorvete da Baccio')
+
+restaurante1.adicionar_no_cardapio(bebida1)
+restaurante1.adicionar_no_cardapio(prato1)
+restaurante1.adicionar_no_cardapio(sobremesa1)
+
 
 def main():
-    Restaurante.listar_restaurantes()
+    restaurante1.exibir_cardapio
     
 
 if __name__ == '__main__':
